bfca4e748ef2a2db490a7f01f2ee14f1f2dfbf83
Marcio Woitek <woitek@usp.br>
2020-09-30 17:13:32 -0300

    Made simple style changes.

--- assignment_3/delete.php
+++ assignment_3/delete.php
@@ -1,34 +1,36 @@
 <?php
-session_start();
-require_once 'universal_functions.php';
-delete_edit_redirect();
-require_once 'delete_edit_view_functions.php';
-$operation = 'delete';
-if ( isset($_POST[$operation]) ) {
-    delete();
-} else {
-    require_once 'pdo.php';
-    $prepared_profile = retrieve_prepared_profile($operation, $pdo);
-}
+    session_start();
+    require_once 'universal_functions.php';
+    delete_edit_redirect();
+    $operation = 'delete';
+    require_once 'delete_edit_view_functions.php';
+    if ( isset($_POST[$operation]) ) {
+        delete();
+    } else {
+        require_once 'pdo.php';
+        $prepared_profile = retrieve_prepared_profile($operation, $pdo);
+    }
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-<?php
-require_once 'bootstrap.php';
-?>
-<title>Marcio Woitek Junior | Delete Profile</title>
-</head>
-<body>
-<div class='container'>
-<h1>Deleting Profile</h1>
-<form method='POST'>
-<p>First Name: <?php echo $prepared_profile['first_name']; ?></p>
-<p>Last Name: <?php echo $prepared_profile['last_name']; ?></p>
-<p><input type='submit' name='delete' value='Delete'>
-<input type='submit' name='cancel' value='Cancel'></p>
-<input type='hidden' name='profile_id' value="<?php echo $_GET['profile_id']; ?>">
-</form>
-</div>
-</body>
+    <head>
+        <?php
+            require_once 'bootstrap.php';
+        ?>
+        <title>Marcio Woitek Junior | Delete Profile</title>
+    </head>
+    <body>
+        <div class='container'>
+            <h1>Deleting Profile</h1>
+            <form method='POST'>
+                <p>First Name: <?php echo $prepared_profile['first_name']; ?></p>
+                <p>Last Name: <?php echo $prepared_profile['last_name']; ?></p>
+                <p>
+                    <input type='submit' name='delete' value='Delete'>
+                    <input type='submit' name='cancel' value='Cancel'>
+                </p>
+                <input type="hidden" name="profile_id" value="<?php echo $_GET['profile_id']; ?>">
+            </form>
+        </div>
+    </body>
 </html>
